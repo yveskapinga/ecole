1a47d9ce409c2386a9c747173cc1c85781a8e4ee
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-03-27 20:31:56 +0100

    ajout page ajout matiere

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -20,9 +20,18 @@ class AdminController extends AbstractController
      /**
      * @Route("/adminMatiere", name="adminMatiere")
      */
-    public function dminMatiere()
+    public function adminMatiere()
     {
         return $this->render('admin/adminMatiere.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+     /**
+     * @Route("/ajoutMatiere", name="ajoutMatiere")
+     */
+    public function ajoutMatiere()
+    {
+        return $this->render('admin/ajoutMatiere.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
